acbb5db2500f448baf082d99c4b788cf8f17e9d8
mercedes.zapata <mercedeszapatafigueroa@gmail.com>
2022-02-13 18:56:18 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let suscripcion= prompt("¿Desea recibir novedades en su email?");
+if(suscripcion =="si"){
+    let email= prompt("Ingese su email: ");
+    alert("Ya estás suscripto");
+}
+else{
+    alert("Lo esperamos proximamente");
+}
