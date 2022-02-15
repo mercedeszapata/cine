0bfb6f5f002d802b62cbf933bc72aabfa7f0bfc9
mercedes.zapata <mercedeszapatafigueroa@gmail.com>
2022-02-15 18:09:06 -0300

    Interacciones con java script

---
+++ peliculas.js
@@ -0,0 +1,15 @@
+let cantidadEntradas= 50;
+let cantidadComprar= parseInt (prompt("Ingrese la cantidad de entradas que quiere comprar"));
+let cantidadRestante= cantidadEntradas - cantidadComprar;
+let edad= prompt("¿Son todos los espectadores mayores de 13 años?");
+while(edad != "no"){
+       let valor= cantidadComprar * 350;
+    alert ("El valor de las entradas es " + valor + ". Gracias por elegirnos");
+    break;
+    
+}
+while(edad =="no"){
+            alert("Esta película está prohibida para menores de 13 años.");
+            break;
+}
+
